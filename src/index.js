35465191281d29b7976ef915fa8ef4f46d8b0ed3
Zaid Shamim <Zaid.Shamim@renold.com>
2019-09-16 21:05:58 +0100

    working

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'antd/dist/antd.css'; 
 
-
+console.log("okay")
 
 ReactDOM.render(
   <Provider store={store}>
