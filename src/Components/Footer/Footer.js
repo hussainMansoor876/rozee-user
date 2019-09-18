46a188b4bee8d55b6582186d92ce8cda8e8392c2
Asad Ikhlas <asadikhlas03@gmail.com>
2019-09-18 22:11:54 +0500

    add changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -41,13 +41,6 @@ class Footer extends Component {
                                         <li><Link to="/wantJob" >Want job</Link></li>
                                     </ul>
                                 </div>
-                                {/* <div className="col-md-6 ">
-                                    <h3 className="footer-heading mb-4 text-white">Categories</h3>
-                                    <ul className="list-unstyled">
-                                        <li><a >Freelancer</a></li>
-                                        <li><a >Permanent</a></li>
-                                    </ul>
-                                </div> */}
                             </div>
                         </div>
                         <div className="col-md-2">
